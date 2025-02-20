3c1c9bc020001c9622d8a897098bb25d6f27b41d
vinicio <vinicio.borja10@gmail.com>
2025-02-20 14:45:26 -0500

    Refactor models to use JPA annotations and Lombok for cleaner code

--- src/main/java/org/borja/springcloud/msvc/usuarios/models/Cliente.java
+++ src/main/java/org/borja/springcloud/msvc/usuarios/models/Cliente.java
@@ -1,4 +1,24 @@
 package org.borja.springcloud.msvc.usuarios.models;
 
-public class Cliente {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "clientes")
+public class Cliente extends Persona {
+
+    @Column(unique = true, nullable = false)
+    private String clienteId;
+
+    @Column(nullable = false)
+    private String contrasena;
+
+    private Boolean estado;
 }
--- src/main/java/org/borja/springcloud/msvc/usuarios/models/Cuenta.java
+++ src/main/java/org/borja/springcloud/msvc/usuarios/models/Cuenta.java
@@ -1,4 +1,24 @@
 package org.borja.springcloud.msvc.usuarios.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "cuentas")
 public class Cuenta {
+    @Id
+    private String numeroCuenta;
+
+    private String tipoCuenta;
+    private Double saldoInicial;
+    private Boolean estado;
 }
